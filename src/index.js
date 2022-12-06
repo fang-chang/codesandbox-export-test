22716ebd8f8070c9cc9ce8711e8543e9900baf24
fang-chang <54119408+fang-chang@users.noreply.github.com>
2022-12-05 16:47:08 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,71 @@
+const colors = ["blue", "green", "white"];
+
+// task 1:  The colors array has 3 items. Let's
+//    use forEach() to log to console every color.
+
+let nums = [35, 482, 201, 98];
+// task 2: Find and print the sum of the nums array using
+//    a forEach loop.
+
+const cats = ["siamese", "calico", "persian", "scottish fold"];
+// task 3: Print the first letter of each cat to the console
+//    using a for of loop.
+
+const primeNums = [2, 3, 5, 7, 11, 13, 17, 19, 23];
+// task 4: Using a for of loop, calculate the square of every
+//    element in the primeNums array.
+
+// task 5: Using a for loop, iterate from 0 to 15. For each
+//    iteration, it will check if the current number is odd or even,
+//    and print it to the console.
+//
+//    Sample Output:
+//     "0 is even"
+//     "1 is odd"
+//     "2 is even"
+
+// task 6: Using a for loop, iterate the integers from 1 to 15.
+//    But for multiples of three print "Fizz" instead of the number
+//    and for the multiples of five print "Buzz". For numbers
+//    which are multiples of both three and five print "FizzBuzz".
+//
+//    Sample output:
+//     1
+//     2
+//     Fizz
+//     4
+//     Buzz
+//     Fizz
+//     7
+//     8
+//     Fizz
+//     Buzz
+//     11
+//     Fizz
+//     13
+//     14
+//     FizzBuzz
+//     -----
+
+// BONUS LOOPS!!
+// Write a JavaScript program to construct the following pattern,
+//    using a nested for loop.
+//
+//    Output:
+//     *
+//     * *
+//     * * *
+//     * * * *
+//     * * * * *
+
+// Write a function called doubleValues that takes an array
+//    as a parameter. The function should return a new array
+//    with all the values in the array doubled
+//
+//    Examples & Sample Output:
+//     doubleValues([1,2,3]) // [2,4,6]
+//     doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
+//
+//    Uncomment the two arrays below to use and test out the function.
+//     const testArr1 = [7, 3, 2, 6, 0];
+//     const testArr2 = [5, 10, 25, 15, 45];
